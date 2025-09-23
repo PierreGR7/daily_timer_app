7d36d5d2640336ffa1dae1349d38d243d8961f1a
Pierre Graef <graef.pierre@gmail.com>
2025-09-23 23:44:42 +0200

    first try library time

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
+import time
+
+def start_timer(task):
+    print(f"Starting {task}...")
+    start = time.time()
+    input("Press Enter to stop ")
+    end = time.time()
+    duration = (end - start) / 60
+    print(f"{task} done: {duration:.2f} minutes")
+    return duration
+
+if __name__ == "__main__":
+    start_timer("Python")
 
