ced7f95c079726e1f201a2724e02622bacd452af
Pierre Graef <graef.pierre@gmail.com>
2025-09-24 23:43:36 +0200

    feat : mvp

--- app.py
+++ app.py
@@ -1,14 +1,33 @@
 import time
+import csv
+import json
+from datetime import datetime
 
 def start_timer(task):
-    print(f"Starting {task}...")
+    print(f"⏳ Starting {task}... (press Enter to stop)")
     start = time.time()
-    input("Press Enter to stop ")
+    input()
     end = time.time()
-    duration = (end - start) / 60
+    duration = (end - start) / 60  # minutes
     print(f"{task} done: {duration:.2f} minutes")
     return duration
 
-if __name__ == "__main__":
-    start_timer("Python")
+def save_history(task, duration):
+    with open("history.csv", "a", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow([datetime.today().date(), task, round(duration, 2)])
 
+if __name__ == "__main__":
+
+    with open("tasks.json", "r") as f:
+        tasks = json.load(f)
+
+    print("📋 Tâches du jour :")
+    for i, t in enumerate(tasks, 1):
+        print(f"{i}. {t['task']} (objectif {t['goal_minutes']} min)")
+
+    choice = int(input("Choisis une tâche (numéro) : "))
+    selected_task = tasks[choice - 1]["task"]
+
+    duration = start_timer(selected_task)
+    save_history(selected_task, duration)
